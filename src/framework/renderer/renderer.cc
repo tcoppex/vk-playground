6a4927e76736d323ec84544d70aac2e6bfde4fc7
Thibault Coppex <tcoppex@proton.me>
2025-04-21 11:39:12 +0200

    load_and_upload : add check when file fails to load.

--- src/framework/renderer/renderer.cc
+++ src/framework/renderer/renderer.cc
@@ -803,11 +803,12 @@ bool Renderer::load_image_2d(std::string_view const& filename, backend::Image &i
 // ----------------------------------------------------------------------------
 
 std::shared_ptr<scene::Resources> Renderer::load_and_upload(std::string_view gltf_filename, scene::Mesh::AttributeLocationMap const& attribute_to_location) {
-  auto R = std::make_shared<scene::Resources>();
-  R->load_from_file(gltf_filename);
-  R->initialize_submesh_descriptors(attribute_to_location);
-  R->upload_to_device(*ctx_ptr_);
-  return R;
+  if (auto R = std::make_shared<scene::Resources>(); R && R->load_from_file(gltf_filename)) {
+    R->initialize_submesh_descriptors(attribute_to_location);
+    R->upload_to_device(*ctx_ptr_);
+    return R;
+  }
+  return nullptr;
 }
 
 /* -------------------------------------------------------------------------- */
