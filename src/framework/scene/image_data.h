e4da56d97f9e1e3912e45e1562fd84f2e6dc674a
Thibault Coppex <tcoppex@proton.me>
2025-08-25 21:32:53 +0200

    [ImageData] Add in-place image constructor.

--- src/framework/scene/image_data.h
+++ src/framework/scene/image_data.h
@@ -25,6 +25,23 @@ struct ImageData {
     load(buffer_data, buffer_size);
   }
 
+  ImageData(uint8_t r, uint8_t g, uint8_t b, uint8_t a, int32_t _width = 1, int32_t _height = 1) {
+    width = _width;
+    height = _height;
+    channels = kDefaultNumChannels;
+    auto* data = static_cast<uint8_t*>(malloc(width * height * channels));
+    int index(0);
+    for (int j=0; j<height; ++j) {
+      for (int i=0; i<width; ++i, ++index) {
+        data[index+0] = r;
+        data[index+1] = g;
+        data[index+2] = b;
+        data[index+3] = a;
+      }
+    }
+    pixels.reset(data);
+  }
+
   bool load(stbi_uc const* buffer_data, uint32_t const buffer_size) {
     auto pixels_data = stbi_load_from_memory(
       buffer_data,
