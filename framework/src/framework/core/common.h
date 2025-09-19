f3199451c4f66a005bce770c2ae3fa5cc9efc9c5
Thibault Coppex <tcoppex@proton.me>
2025-09-19 22:20:45 +0200

    Add shared structures ArgList.

--- framework/src/framework/core/common.h
+++ framework/src/framework/core/common.h
@@ -25,7 +25,6 @@ using uint = uint32_t; //
 #include "framework/core/logger.h"
 
 /* -------------------------------------------------------------------------- */
-
 /* -- c++ features -- */
 
 template<typename T, typename U>
@@ -34,7 +33,19 @@ concept DerivedFrom = std::is_base_of_v<U, T>;
 template<typename T>
 concept SpanConvertible = requires(T t) { std::span(t); };
 
+// ----------------------------------------------------------------------------
 /* -- custom type -- */
+
+// Holds an abstraction over the main argument list.
+struct ArgList : public std::span<char*> {
+  ArgList(int argc, char *argv[])
+    : std::span<char*>{argv, static_cast<size_t>(argc)}
+  {}
+
+  ArgList()
+    : ArgList(0, nullptr)
+  {}
+};
 
 // Wrap an array to accept enum class as indexer.
 // Original code from Daniel P. Wright.
@@ -57,6 +68,7 @@ class EnumArray : public std::array<T, static_cast<size_t>(Indexer::kCount)> {
 
 struct NullType {};
 
+// ----------------------------------------------------------------------------
 /* -- constants -- */
 
 static constexpr uint32_t kInvalidIndexU32{ std::numeric_limits<uint32_t>::max() };
