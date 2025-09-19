45c87606f72dd99eb471710a27572678295892bb
Thibault Coppex <tcoppex@proton.me>
2025-09-19 20:54:51 +0200

    [Platform] Add GPU high-performance hint for Windows (NVIDIA Optimus / AMD PowerXpress)
    - Ensures the application prefers the discrete GPU on laptops with switchable graphics

---
+++ framework/src/framework/core/platform/desktop/gpu_selection_hints.cc
@@ -0,0 +1,16 @@
+/* -------------------------------------------------------------------------- */
+
+// Choose the best available GPU.
+#if defined(WIN32)
+extern "C" {
+
+// http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
+__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+
+// https://gpuopen.com/learn/amdpowerxpressrequesthighperformance/
+__declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 0x00000001;
+
+}
+#endif
+
+/* -------------------------------------------------------------------------- */
