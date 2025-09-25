20a9d2d1d567c8819089327c9e4711041d3581f3
Thibault Coppex <tcoppex@proton.me>
2025-09-25 14:20:42 +0200

    [Logger] Add libfmt and a Logger singleton wrapping it.

--- framework/src/framework/core/logger.h
+++ framework/src/framework/core/logger.h
@@ -3,6 +3,8 @@
 
 /* -------------------------------------------------------------------------- */
 
+#include "framework/core/singleton.h"
+
 extern "C" {
 #if defined(ANDROID)
 #include <android/log.h>
@@ -10,6 +12,150 @@ extern "C" {
 #include <cstdio>
 #endif
 }
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+
+#include "fmt/core.h" // (c++20 format require gcc13+)
+
+/* -------------------------------------------------------------------------- */
+
+//
+// A colored logger that could be used inside loops to print messages once.
+//
+//  Type of logs :
+//    * Message    : white, not hashed (will be repeated).
+//    * Info       : blue, hashed (will not be repeated).
+//    * Warning    : yellow, hashed, used in stats.
+//    * Error      : bold red, hashed, display file and line, used in stats.
+//    * FatalError : flashing red, not hashed, exit program instantly.
+//
+class Logger : public Singleton<Logger> {
+  friend class Singleton<Logger>;
+
+ public:
+  static std::string TrimFilename(std::string const& filename) {
+    return filename.substr(filename.find_last_of("/\\") + 1);
+  }
+
+  enum class LogType {
+    Message,
+    Info,
+    Warning,
+    Error,
+    FatalError
+  };
+
+  ~Logger() {
+    // displayStats();
+  }
+
+  template<typename... Args>
+  bool log(
+    char const* file, char const* fn, int line,
+    bool useHash, LogType type,
+    std::string_view fmt, Args&&... args
+  ) {
+    // Clear the local stream and retrieve the full current message.
+    out_.str({});
+    out_ << fmt::vformat(fmt, fmt::make_format_args(std::forward<Args>(args)...));
+
+    // Trim filename for display.
+    std::string filename(file);
+    filename = filename.substr(filename.find_last_of("/\\") + 1);
+
+    // Check the message has not been registered yet.
+    if (useHash) {
+      auto const key = std::string(/*filename + std::to_string(line) +*/ out_.str());
+      if (0u < error_log_.count(key)) {
+        return false;
+      }
+      error_log_[key] = true;
+    }
+
+    // Prefix.
+    switch (type) {
+      case LogType::Message:    std::cerr << "\x1b[0;29m";
+        break;
+      case LogType::Info:       std::cerr << "\x1b[0;36m";
+        break;
+      case LogType::Warning:    std::cerr << "\x1b[3;33m";
+        ++warning_count_;
+        break;
+      case LogType::Error:      std::cerr << "\x1b[1;31m[Error] ";
+        ++error_count_;
+        break;
+      case LogType::FatalError: std::cerr << "\x1b[5;31m[Fatal Error]\x1b[0m\n\x1b[0;31m ";
+        break;
+    }
+
+    std::cerr << out_.str();
+
+    // Suffix.
+    switch (type) {
+      case LogType::Error:
+      case LogType::FatalError:
+        std::cerr <<  "\n(" << filename << " " << fn << " L." << line << ")\n";
+        break;
+
+      default:
+        break;
+    }
+
+    std::cerr << "\x1b[0m\n";
+
+    return true;
+  }
+
+  template<typename... Args>
+  void message(char const* file, char const* fn, int line, fmt::format_string<Args...> fmt, Args&&... args) {
+    log(file, fn, line, false, LogType::Message, fmt::vformat(fmt, fmt::make_format_args(args...)));
+  }
+
+  template<typename... Args>
+  void info(char const* file, char const* fn, int line, fmt::format_string<Args...> fmt, Args&&... args) {
+    log(file, fn, line, true, LogType::Info, fmt::vformat(fmt, fmt::make_format_args(args...)));
+  }
+
+  template<typename... Args>
+  void warning(char const* file, char const* fn, int line, std::string_view fmt, Args&&... args) {
+    log(file, fn, line, true, LogType::Warning, fmt::vformat(fmt, fmt::make_format_args(args...)));
+  }
+
+  template<typename... Args>
+  void error(char const* file, char const* fn, int line, fmt::format_string<Args...> fmt, Args&&... args) {
+    log(file, fn, line, true, LogType::Error, fmt::vformat(fmt, fmt::make_format_args(args...)));
+  }
+
+  template<typename... Args>
+  void fatal_error(char const* file, char const* fn, int line, fmt::format_string<Args...> fmt, Args&&... args) {
+    log(file, fn, line, false, LogType::FatalError, fmt::vformat(fmt, fmt::make_format_args(args...)));
+    //throw std::runtime_error("Fatal error");
+    // std::terminate();
+    std::exit(EXIT_FAILURE);
+  }
+
+ private:
+  void displayStats() {
+#ifndef NDEBUG
+    if ((warning_count_ > 0) || (error_count_ > 0)) {
+      std::cerr << "\n"
+        "\x1b[7;38m================= Logger stats =================\x1b[0m\n" \
+        " * Warnings : " << warning_count_ << std::endl <<
+        " * Errors   : " << error_count_ << std::endl <<
+        "\x1b[7;38m================================================\x1b[0m\n\n"
+        ;
+    }
+#endif // NDEBUG
+  }
+
+  std::stringstream out_{};
+  std::unordered_map<std::string, bool> error_log_{};
+  int32_t warning_count_{};
+  int32_t error_count_{};
+};
 
 /* -------------------------------------------------------------------------- */
 
@@ -33,13 +179,24 @@ extern "C" {
 #define LOGW(...) fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n");
 #define LOGE(...) fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n");
 
+// #define LOGV(...)   Logger::Get().message( __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+// #define LOGD(...)   Logger::Get().message( __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+// #define LOGI(...)   Logger::Get().info   ( __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+// #define LOGW(...)   Logger::Get().warning( __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+// #define LOGE(...)   Logger::Get().error  ( __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+
 #endif
 
+// ----------------------------------------------------------------------------
+
 #ifdef NDEBUG
+
 #undef LOGV
-#undef LOGD
 #define LOGV(...)
+
+#undef LOGD
 #define LOGD(...)
+
 #endif
 
 /* -------------------------------------------------------------------------- */
